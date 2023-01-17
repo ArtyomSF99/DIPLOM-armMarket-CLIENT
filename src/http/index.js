b92b4cf730e618c890a8f884418e5c537f3d5c8a
ArtyomSF <artemgulgulan@gmail.com>
2023-01-17 21:35:29 +0400

    commit13

--- src/http/index.js
+++ src/http/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AuthService from "../services/AuthService";
 
-export const API_URL = process.env.SERVER_URL || `http://localhost:5000`
+export const API_URL ="https://diplom-arm-market-server.onrender.com" // `http://localhost:5000`
 //`https://diplom-arm-market-server.onrender.com`
 
 const $api = axios.create({
