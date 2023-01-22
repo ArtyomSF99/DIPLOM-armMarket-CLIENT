8b6ae51b5a675265e6f7280b2cd14a918c58d2a3
ArtyomSF <artemgulgulan@gmail.com>
2023-01-22 20:33:03 +0400

    commit17

--- src/components/chat/MyChat.jsx
+++ src/components/chat/MyChat.jsx
@@ -11,7 +11,9 @@ import classes from './Chat.module.css'
 import ChatList from './ChatList';
 
 function MyChat({ senderId, receiverId }) {
-  const [socket] = useState(socketio(`https://diplom-arm-market-server.onrender.com:5001`));
+  const [socket] = useState(socketio(`https://diplom-arm-market-server.onrender.com:443`,{
+    timeout:0
+  }));
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
   const [isLoading, setIsLoading] = useState(true)
